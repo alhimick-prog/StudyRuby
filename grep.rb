97ac5f37c5ede7df7fac06fc8ea82c4eb7ff1c3f
alhimick-prog <alhimick96@yahoo.com>
2021-09-07 18:06:55 +0300

    grep&matrix_fix

--- grep.rb
+++ grep.rb
@@ -19,8 +19,8 @@ def analysis_of_arguments(arguments)
   fail 'Not enough arguments.' if arguments.size < 2
   arg_hash = { '-n' => false, '-l' => false, '-i' => false, '-v' => false, '-x' => false, 'curr_arg' => 0 }
   supporting_flags = [ '-n', '-l', '-i', '-v', '-x']
+  is_it_flag = false
   arguments.each_with_index do |value, arg_numb|
-    is_it_flag = false
     supporting_flags.each_with_index do |flag, index|
       if value == flag
         supporting_flags[index] = ''
@@ -30,58 +30,170 @@ def analysis_of_arguments(arguments)
     end
     if is_it_flag
       is_it_flag = false
+      arg_hash['curr_arg'] += 1
     else
-      arg_hash['curr_arg'] = arg_numb
-      break
+      supporting_flags = []
     end
   end
   arg_hash
 end
+
 def determ_pattern(arguments, arg_hash)
   pattern = arguments[arg_hash['curr_arg']]
   arg_hash['curr_arg'] += 1
   pattern
 end
+
 def determ_filenames(arguments, arg_hash)
-  filenames = Array.new
+  filenames = []
   counter = 0
-  for i in arg_hash['curr_arg']..(arguments.size - 1) do
-    filenames[counter] = arguments[arg_hash['curr_arg']]
-    arg_hash['curr_arg'] +=1
-    counter += 1
+  (arg_hash['curr_arg']..(arguments.size - 1)).each do |i|
+    filenames[counter] = arguments[i]
+    counter +=1
   end
   filenames
 end
-def print_line_number #-n
-end
-def print_filenames #-l
-end
-def insensitivite #-i
-end
-def invert #-v
-end
-def all_line #-x
-end
+
 def file_check(filenames)
-  fail "File #{filenames[i]} not found"
+  fail 'No file name found.' if filenames.empty?
+  filenames.each do |filename|
+    fail "File #{filename} not found" if !(File::exists?(filename))
+  end
 end
-def filework(filenames)
+
+def filework(filenames, arg_hash, pattern)
+  many_files = false
+  many_files = true if filenames.size > 1
   for i in 0..(filenames.size - 1) do
-    File.open(filenames[i], 'r') |curr_file| do
+    file_content = IO.readlines(filenames[i])
+    if arg_hash['-l']
+      puts filenames[i] if print_filenames?(file_content, arg_hash, pattern, filenames[i])
+    else
+      puts "#{filenames[i]}:" if many_files && print_filenames?(file_content, arg_hash, pattern, filenames[i])
+      if arg_hash['-x']
+        all_line(file_content, arg_hash, pattern, filenames[i])
+      else
+        pattern_in_line(file_content, arg_hash, pattern, filenames[i])
+      end
     end
   end
 end
 
+def print_filenames?(file_content, arg_hash, pattern, filename) #-l true
+  match = false
+  if arg_hash['-x']
+    match = filename_all_line(file_content, arg_hash, pattern)
+  else
+    match = filename_pattern_in_line(file_content, arg_hash, pattern)
+  end
+  match
+end
+
+def filename_all_line(file_content, arg_hash, pattern) #-l & -x true
+  match = false
+  counter = 0
+  if arg_hash['-i']
+    file_content.each do |line|
+      if line.downcase.chomp == pattern.downcase.chomp
+        match = true
+        counter += 1
+      end
+    end
+  else
+    file_content.each do |line|
+      if line.chomp == pattern.chomp
+        match = true
+        counter += 1
+      end
+    end
+  end
+  match = true if arg_hash['-v']
+  match = false if arg_hash['-v'] && counter == file_content.size
+  match
+end
+
+def filename_pattern_in_line(file_content, arg_hash, pattern) #-l true -x false
+  match = false
+  counter = 0
+  if arg_hash['-i']
+    file_content.each do |line|
+      if line.downcase =~ /#{pattern.downcase}/
+        match = true
+        counter += 1
+      end
+    end
+  else
+    file_content.each do |line|
+      if line =~ /#{pattern}/
+        match = true
+        counter += 1
+      end
+    end
+  end
+  match = true if arg_hash['-v']
+  match = false if arg_hash['-v'] && counter == file_content.size
+  match
+end
+
+def all_line(file_content, arg_hash, pattern, filename) #-l false -x true
+  if arg_hash['-i']
+    file_content.each_with_index do |line, line_numb|
+      match = false
+      match = true if line.downcase.chomp == pattern.downcase.chomp
+      match = !match if arg_hash['-v']
+      if match && arg_hash['-n']
+        puts "#{line_numb + 1}. #{line}"
+      elsif match
+        puts line
+      end
+    end
+  else
+    file_content.each_with_index do |line, line_numb|
+      match = false
+      match = true if line.chomp == pattern.chomp
+      match = !match if arg_hash['-v']
+      if match && arg_hash['-n']
+        puts "#{line_numb + 1}. #{line}"
+      elsif match
+        puts line
+      end
+    end
+  end
+end
+
+def pattern_in_line(file_content, arg_hash, pattern, filename) #-l & -x false
+  if arg_hash['-i']
+    file_content.each_with_index do |line, line_numb|
+      match = false
+      match = true if line.downcase =~ /#{pattern.downcase}/
+      match = !match if arg_hash['-v']
+      if match && arg_hash['-n']
+        puts "#{line_numb + 1}. #{line}"
+      elsif match
+        puts line
+      end
+    end
+  else
+    file_content.each_with_index do |line, line_numb|
+      match = false
+      match = true if line =~ /#{pattern}/
+      match = !match if arg_hash['-v']
+      if match && arg_hash['-n']
+        puts "#{line_numb + 1}. #{line}"
+      elsif match
+        puts line
+      end
+    end
+  end
+end
 
 arguments = ARGV
-puts arguments.to_s
+#puts "Input agument:\n#{arguments.to_s}"
 arg_hash = analysis_of_arguments(arguments)
-puts arg_hash
+#puts "After analis arg_hash:\n#{arg_hash}"
 pattern = determ_pattern(arguments, arg_hash)
-puts pattern
-puts arg_hash
+#puts "Pattern: #{pattern}"
 filenames = determ_filenames(arguments, arg_hash)
-puts filenames.to_s
-puts arg_hash
-
-#puts arguments[analysis_of_arguments(arguments)['curr_arg']]
+#puts "Filenames:\n#{filenames.to_s}"
+file_check(filenames)
+filework(filenames, arg_hash, pattern)
